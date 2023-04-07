13ea8572b0392bf29ccf3b5e966ad199418ec2df
Neal <neal99185@gmail.com>
2023-04-07 22:03:15 +0800

    fix error: question2 repeat.

--- src/HOME/components/QuestionList.js
+++ src/HOME/components/QuestionList.js
@@ -4,7 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 const QuestionList = () => {
   const questionArr = [
     {
-      question: "1: 個人簡介",
+      question: 
+        "1: 個人簡介",
       answer:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
     },
@@ -15,18 +16,14 @@ const QuestionList = () => {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
     },
     {
-      question: "3: 如果參與這個計畫，會怎麼安排學習時間？",
+      question: 
+        "3: 如果參與這個計畫，會怎麼安排學習時間？",
       answer:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
     },
     {
-      question:
-        "2: 為了成為軟體工程師，曾做過什麼努力？若有具體專案作品請分享給我們。",
-      answer:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
-    },
-    {
-      question: "4: 是否有想要加入的軟體公司？為什麼想加入該公司？",
+      question: 
+        "4: 是否有想要加入的軟體公司？為什麼想加入該公司？",
       answer:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
     },
@@ -43,7 +40,8 @@ const QuestionList = () => {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
     },
     {
-      question: "7:其他想要對我們說的事情？",
+      question: 
+        "7:其他想要對我們說的事情？",
       answer:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sunt. At quam ipsam necessitatibus. In obcaecati aperiam amet! Odit voluptatibus voluptate consequuntur totam tempora incidunt itaque eos. Natus, ratione vero..(使用db.json)",
     },
