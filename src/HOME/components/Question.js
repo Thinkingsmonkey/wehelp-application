9d2bb5ea06886c27979ab40d138ddc5a7c3c481c
Neal <neal99185@gmail.com>
2023-04-10 20:53:39 +0800

    add key to question components's props answer, change string to array in answer

--- src/HOME/components/Question.js
+++ src/HOME/components/Question.js
@@ -1,12 +1,38 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 const Question = ({ question, answer }) => {
+  const answerWithKeys = answer.map((element) => {
+    if (React.isValidElement(element)) {
+      return React.cloneElement(element, { key: uuidv4() });
+    }
     return (
-      <div className="question">
-        <h2 className="question-title">
-          {question} 
-        </h2>
-        <p className="question-answer">{answer}</p>
-      </div>
+      <React.Fragment key={uuidv4()}>
+        {element}
+      </React.Fragment>
     );
-  };
+  });
+
+  return (
+    <div className="question">
+      <h2 className="question-title">{question}</h2>
+      <div className="question-answer">
+        {React.Children.toArray(answerWithKeys)}
+      </div>
+    </div>
+  );
+};
 
 export default Question;
+
+// const Question = ({ question, answer }) => {
+//     return (
+//       <div className="question">
+//         <h2 className="question-title">
+//           {question} 
+//         </h2>
+//         <div className="question-answer">{answer}</div>
+//       </div>
+//     );
+//   };
+
+// export default Question;
